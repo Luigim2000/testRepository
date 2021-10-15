34db3ad29f037a725c916e7fa6dfb5b526ae1d2a
Luigim2000 <92590219+Luigim2000@users.noreply.github.com>
2021-10-15 16:43:34 +0200

    test

---
+++ src/main/java/it/unical/demacs/IngSw/App.java
@@ -0,0 +1,16 @@
+package it.unical.demacs.IngSw;
+
+import org.joda.time.DateTime;
+
+import com.sun.tools.javac.util.StringUtils;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DateTime joda=new DateTime();
+        System.out.println(joda.toString());
+        System.out.println(StringUtils.toUpperCase("Hello world"));
+    }
+}
